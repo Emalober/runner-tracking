57611f8ca6d35b85ed02c591225cb1c3d523e03c
Ezequiel Maloberti <emaloberti@technisys.com>
2020-08-16 00:49:32 -0300

    Handling the Map's Lifecycle

--- app/src/main/java/com/ar/maloba/runnertracking/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/ar/maloba/runnertracking/ui/fragments/TrackingFragment.kt
@@ -1,8 +1,62 @@
 package com.ar.maloba.runnertracking.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.ar.maloba.runnertracking.R
+import com.ar.maloba.runnertracking.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
+import kotlinx.android.synthetic.main.tracking_fragment.*
 
 class TrackingFragment : Fragment(R.layout.tracking_fragment) {
 
+    private val viewModel: MainViewModel by viewModels()
+
+    private var map: GoogleMap? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync {
+            map = it
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        mapView?.onDestroy()
+//    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+
+    }
 }
